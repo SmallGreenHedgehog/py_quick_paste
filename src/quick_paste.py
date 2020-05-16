de47fe83b3cebe16dba10bea0a873e6f01e23380
mrJill <jill.overlord@gmail.com>
2020-05-16 18:15:04 +0300

    Удалил TODO реализованного функционала.

--- src/quick_paste.py
+++ src/quick_paste.py
@@ -108,7 +108,6 @@ class EditWindowForm(QtWidgets.QWidget):
         return result
 
     def set_comb(self):
-        # TODO добавить поддержку Catalina (не работает обновление надписи)
         self.__act_comb = tray_icon_window.keys.get_combination()
         self.ui.label_comb.setText(str(self.__act_comb))
         self.ui.label_comb.repaint()
